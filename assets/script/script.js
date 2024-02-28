bdd4085846756524ad46d5dd9c90826d727a1eba
hhhtlcu <121258756+hhhtlcu@users.noreply.github.com>
2024-02-29 05:04:50 +0800

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,40 @@
+/* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
+  window.onscroll = function() {headerShadow()};
+
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+
+    } else {
+
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+
+    }
+  }
+  
+/* ----- TYPING EFFECT ----- */
+ var typingEffect = new Typed(".typedText",{
+    strings : ["Programmer","Networker","Developer"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+window.addEventListener('scroll', scrollActive)
